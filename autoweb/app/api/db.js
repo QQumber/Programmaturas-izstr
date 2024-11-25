75f9e2a7cc0b91763c0240a73dae6814d4d15f98
ValtersVirs <valtersvirs@gmail.com>
2024-11-25 18:23:34 +0200

    DB connection, login, register

---
+++ autoweb/app/api/db.js
@@ -0,0 +1,14 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT,
+});
+
+module.exports = {
+    query: (text, params) => pool.query(text, params), 
+    pool,
+};
---
+++ autoweb/app/api/users/login/route.js
@@ -0,0 +1,50 @@
+import bcrypt from 'bcryptjs';
+import { query } from '../../db';
+
+export async function POST(req) {
+    try {
+    const { username, password } = await req.json();
+
+    // Check if the required fields are provided
+    if (!username || !password) {
+        return new Response(JSON.stringify({ error: 'Missing username or password' }), {
+            status: 400,
+            headers: { 'Content-Type': 'application/json' },
+        });
+    }
+
+    // Check if the username exists
+    const checkUsername = await query('SELECT * FROM user_profiles WHERE username = $1', [username]);
+    if (checkUsername.rows.length === 0) {
+        return new Response(JSON.stringify({ error: 'Invalid username or password' }), {
+            status: 401,
+            headers: { 'Content-Type': 'application/json' },
+        });
+    }
+
+    const user = checkUsername.rows[0];
+
+    // Compare passwords
+    const passwordMatch = await bcrypt.compare(password, user.password_hash);
+    if (!passwordMatch) {
+        return new Response(JSON.stringify({ error: 'Invalid username or password' }), {
+            status: 401,
+            headers: { 'Content-Type': 'application/json' },
+        });
+    }
+
+    // Successful login
+    const { password_hash, ...userData } = user
+    console.log("test login source");
+    return new Response(JSON.stringify({ message: 'Login successful', user: userData }), {
+        status: 200,
+        headers: { 'Content-Type': 'application/json' },
+    });
+    } catch (error) {
+        console.error('Login Error:', error);
+        return new Response(JSON.stringify({ error: 'Login failed' }), {
+            status: 500,
+            headers: { 'Content-Type': 'application/json' },
+        });
+    }
+}
