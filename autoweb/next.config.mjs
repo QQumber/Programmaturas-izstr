03ea29d919e3c242fc848013eb0f50c42863b6b3
Kārlis Kociņš <karlis.kocins@edu.rtu.lv>
2024-12-01 23:36:44 +0200

    Refactor Next.js configuration by removing unused settings
    - Removed output configuration and basePath settings for production.
    - Eliminated unoptimized image handling to streamline the Next.js setup.

--- autoweb/next.config.mjs
+++ autoweb/next.config.mjs
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/Programmaturas-izstr' : '',
-  images: {
-    unoptimized: true,
-  },
   async headers() {
     return [
       {
