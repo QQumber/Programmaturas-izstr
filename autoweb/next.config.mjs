f3f1c885decb6620410ef71b9ff415679f7577e6
Kārlis Kociņš <karlis.kocins@edu.rtu.lv>
2024-12-01 22:57:54 +0200

    Update GitHub Actions workflow and Next.js configuration
    - Enabled cancellation of in-progress deployments in the GitHub Actions workflow.
    - Simplified package manager detection by directly using npm for dependency installation and build commands.
    - Updated caching strategy to use npm's package-lock.json for better performance.
    - Configured Next.js to export static files, set a base path for production, and disabled image optimization for improved build efficiency.

--- autoweb/next.config.mjs
+++ autoweb/next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/Programmaturas-izstr' : '',
+  images: {
+    unoptimized: true,
+  },
   async headers() {
     return [
       {
